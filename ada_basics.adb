56421630b28e45e2c2bdf117c170e614315f0888
dc105297 <davidchason@gmail.com>
2017-06-21 02:56:44 -0400

    Ada basics

---
+++ ada_basics.adb
@@ -0,0 +1,6 @@
+with Messages_To_User;
+procedure Main is
+
+begin
+    Messages_To_User.Print_Hello_World;
+end Main;
---
+++ messages_to_user.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+package body Messages_To_User is
+
+    procedure Print_Hello_World is
+    begin
+        Put_Line("Hello World!");
+    end Print_Hello_World;
+
+end Messages_To_User;
